7952b2710eb82ae74c4f2e7336d212c7daed8436
PilgrimLyieu <PilgrimLyieu@outlook.com>
2023-09-23 14:35:03 +0800

    Complete "2023-1-types-io #D-Time.c"

---
+++ OJ/2023-1-types-io/D-Time.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char month[10], weekday[10];
+    int day, year, hour, minute, second;
+    scanf("%s %d %d %s %d %d %d", month, &day, &year, weekday, &hour, &minute, &second);
+    printf("%.3s %.3s %.2d %.2d:%.2d:%.2d %.4d", weekday, month, day, hour, minute, second, year);
+    return 0;
+}
