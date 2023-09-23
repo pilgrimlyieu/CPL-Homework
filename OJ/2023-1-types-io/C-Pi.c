ca213649bc18eb5e8545fb89c400906c896b0f3e
PilgrimLyieu <PilgrimLyieu@outlook.com>
2023-09-23 11:20:02 +0800

    Complete "2023-1-types-io: C-Pi"

---
+++ OJ/2023-1-types-io/C-Pi.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    printf("%.15lf\n", log(pow(640320, 3) + 744) / sqrt(163));
+    printf("%.15lf", 16 * atan(0.2) - 4 * atan(1.0 / 239));
+    return 0;
+}
