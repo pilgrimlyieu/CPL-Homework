bdbd07acfbeb55d91bf9ea9cbd2d6854bca5bd1b
PilgrimLyieu <PilgrimLyieu@outlook.com>
2023-10-09 13:33:35 +0800

    Complete "2023-2-if-for-array #A-single"

---
+++ OJ/2023-2-if-for-array/A-single.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int n, temp;
+    scanf("%d", &n);
+    int array[10 * n + 1];
+    for (int i = 0; i < 2 * n - 1; i++) {
+        scanf("%d", &temp);
+        array[temp] = (array[temp] != -1) ? -1 : -2;
+    }
+    for (int i = 1; i <= 10 * n; i ++) {
+        if (array[i] == -1) {
+            printf("%d", i);
+            return 0;
+        }
+    }
+}
