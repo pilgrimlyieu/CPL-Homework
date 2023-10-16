3308ca5766af696dabadbc6c350549ebe4bd3e21
PilgrimLyieu <PilgrimLyieu@outlook.com>
2023-10-16 19:32:29 +0800

    Optimize "2023-3-for-a-while #F, #H"
    Optimize:
    1. "2023-3-for-a-while #F-bubble-sort"
    2. "2023-3-for-a-while #H-palindrome-2"

--- OJ/2023-3-for-a-while/F-bubble-sort.c
+++ OJ/2023-3-for-a-while/F-bubble-sort.c
@@ -4,13 +4,12 @@ int main(void) {
     int T;
     scanf("%d", &T);
     for (int i = 0; i < T; i++) {
-        int n;
+        int n, max = 1;
         scanf("%d", &n);
         int a[n];
         for (int j = 0; j < n; j++) {
             scanf("%d", &a[j]);
         }
-        int max = 1;
         for (int k = 0; k < n - 1; k++) {
             max = (a[k] > max) ? a[k] : max;
         }
--- OJ/2023-3-for-a-while/H-palindrome-2.c
+++ OJ/2023-3-for-a-while/H-palindrome-2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-    int n, times[100] = { 0 }, will[100] = { 0 };
+    int n, times[100] = { 0 };
     scanf("%d", &n);
     char str[n];
     int use[n];
@@ -11,11 +11,11 @@ int main(void) {
             if (str[i] == str[n - 1 - i]) {
                 if (2 * i < n) {
                     for (int j = 0; j < 100; j++) {
-                        if (times[j] < 2 && will[j] < 2 && times[j] + will[j] < 2) {
+                        if (times[j] < 2) {
                             printf("%.2d", j);
                             times[j]++;
+                            times[10 * (j % 10) + j / 10]++;
                             use[i] = j;
-                            will[10 * (j % 10) + j / 10]++;
                             break;
                         }
                     }
