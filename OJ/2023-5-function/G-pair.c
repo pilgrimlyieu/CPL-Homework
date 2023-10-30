45c97ce47a8ce2c111ee5cccbf53d5caff6b6b5f
PilgrimLyieu <PilgrimLyieu@outlook.com>
2023-10-30 17:23:24 +0800

    Complete "2023-5-function #G-pair"

--- OJ/2023-5-function/G-pair.c
+++ OJ/2023-5-function/G-pair.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void sort(int array[], int low, int high) {
+void Sort(int array[], int low, int high) {
     if (low >= high)
         return;
     int i = low - 1, j = high + 1, pivot = array[(low + high) / 2];
@@ -17,8 +17,8 @@ void sort(int array[], int low, int high) {
             array[j] = temp;
         }
     }
-    sort(array, low, j);
-    sort(array, j + 1, high);
+    Sort(array, low, j);
+    Sort(array, j + 1, high);
 }
 
 int main(void) {
@@ -29,21 +29,31 @@ int main(void) {
         scanf("%d", &a[i]);
     for (int j = 0; j < m; j++)
         scanf("%d", &b[j]);
-    sort(b, 0, m - 1);
-    for (int i = n - 1; i >= 0; i--) {
-        if (a[i] > t)
+    Sort(b, 0, m - 1);
+    int a_index = n - 1;
+    while (a_index > 0) {
+        int b_low = 0, b_high = m - 1;
+        if (a[a_index] > t || a[a_index] == a[a_index + 1]) {
+            a_index--;
             continue;
-        for (int j = m - 1; j >= 0; j--) {
-            if (b[j] > t)
+        }
+        while (b_low <= b_high) {
+            int b_mid = (b_low + b_high) / 2;
+            if (b[b_mid] > t) {
+                b_high = b_mid - 1;
                 continue;
-            int sum = a[i] + b[j];
+            }
+            int sum = a[a_index] + b[b_mid];
             if (sum <= t) {
                 int minus = t - sum;
-                if (minus < min)
+                if (minus <= min)
                     min = minus;
-                break;
+                b_low = b_mid + 1;
             }
+            else
+                b_high = b_mid - 1;
         }
+        a_index--;
     }
     printf("%d", (min == t) ? -1 : min);
     return 0;
