5478d7c6e499566438b9a69f839e9c9c490c9c80
PilgrimLyieu <PilgrimLyieu@outlook.com>
2023-11-14 14:38:50 +0800

    Complete "2023-6-recursion #A-pascal"

---
+++ OJ/2023-6-recursion/A-pascal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+long long binomial(int n, int i) {
+    if (i == 0 || n == i)
+        return 1;
+    else
+        return binomial(n - 1, i - 1) + binomial(n - 1, i);
+}
+
+int main(void) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%lld", binomial(a - 1, b - 1));
+}
