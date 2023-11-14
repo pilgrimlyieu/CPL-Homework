ae83f0b9f4f99aa99b315d4d0bfa18c394c9e442
PilgrimLyieu <PilgrimLyieu@outlook.com>
2023-11-14 14:48:38 +0800

    Complete "2023-6-recursion #C-poi"

---
+++ OJ/2023-6-recursion/C-poi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int poi(int n, int a[]) {
+    if (n == a[n])
+        return n;
+    else
+        return poi(a[n], a);
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    int a[n + 1];
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+    for (int i = 1; i <= n; i++)
+        printf("%d ", poi(i, a));
+}
