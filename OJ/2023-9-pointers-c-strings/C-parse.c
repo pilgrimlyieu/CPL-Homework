3236335d83a62fb28ff0c55eadb84915e4a0411f
PilgrimLyieu <PilgrimLyieu@outlook.com>
2023-12-05 14:56:08 +0800

    Complete "2023-9-pointers-c-strings #C-parse"

---
+++ OJ/2023-9-pointers-c-strings/C-parse.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int valid(char param, const char *parse, int len) {
+    for (int i = 0; i < len; i++) {
+        if (*(parse + i) == param) {
+            if (*(parse + i + 1) == ':')
+                return 2;
+            else
+                return 1;
+        }
+    }
+    return 0;
+}
+
+int main(void) {
+    char *parse = malloc(128 * sizeof(parse)), *token = malloc(1024 * sizeof(token)), *name = malloc(1024 * sizeof(name)), *msg = malloc(5300604 * sizeof(msg));
+    scanf("%s", parse);
+    int len = strlen(parse);
+    scanf("%s", name);
+    strcat(msg, name);
+    strcat(msg, "\n");
+    while (scanf("%s", token) != EOF) {
+        if (token[0] != '-')
+            continue;
+        switch (valid(token[1], parse, len)) {
+            case 0:
+                printf("%s: invalid option -- '%c'\n", name, token[1]);
+                goto finish;
+            case 1:
+                strcat(msg, token + 1);
+                strcat(msg, "\n");
+                break;
+            case 2: {
+                char *param = malloc(1024 * sizeof(param));
+                if (scanf("%s", param) == EOF) {
+                    printf("%s: option requires an argument -- '%c'\n", name, token[1]);
+                    goto finish;
+                }
+                else {
+                    strcat(msg, token + 1);
+                    strcat(msg, "=");
+                    strcat(msg, param);
+                    strcat(msg, "\n");
+                }
+                break;
+            }
+        }
+    }
+    printf("%s", msg);
+    finish:
+    return 0;
+}
